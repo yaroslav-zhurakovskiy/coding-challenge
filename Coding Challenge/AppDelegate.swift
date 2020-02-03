3a039ecae2195696f522421da1a9408c91900ed2
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2020-02-03 22:12:54 +0200

    Create a basic project

---
+++ Coding Challenge/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Coding Challenge
+//
+//  Created by Yaroslav Zhurakovskiy on 03.02.2020.
+//  Copyright © 2020 Yaroslav Zhurakovskiy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
