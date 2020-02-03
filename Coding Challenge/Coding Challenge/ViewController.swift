6a33dded1a5fa726d279adbe77b886d9a4a5f9b0
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2020-02-03 22:12:28 +0200

    Update .gitignore

---
+++ Coding Challenge/Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coding Challenge
+//
+//  Created by Yaroslav Zhurakovskiy on 03.02.2020.
+//  Copyright © 2020 Yaroslav Zhurakovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
